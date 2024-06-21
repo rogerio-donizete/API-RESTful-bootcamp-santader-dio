2e3def153feb8872d98f614ee7b39dadbf42807a
Bowie <marketing@letscode.com.br>
2024-06-21 14:04:29 -0300

    iniciado o projeto de api RESTful para o bootcamp santander 2024

---
+++ src/main/java/me/dio/santander_bootcamp_java/SantanderBootcampJava2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampJava2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampJava2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander_bootcamp_java/SantanderBootcampJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
