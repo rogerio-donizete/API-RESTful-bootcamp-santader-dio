f4d0484b9d2bc6240ed5bfa343b8a93c41e713a6
Bowie <marketing@letscode.com.br>
2024-06-21 15:04:26 -0300

    Criado os atributos e os getters e setters das classes model.

--- src/main/java/me/dio/santander_bootcamp_java/domain/model/User.java
+++ src/main/java/me/dio/santander_bootcamp_java/domain/model/User.java
@@ -1,4 +1,61 @@
 package me.dio.santander_bootcamp_java.domain.model;
 
+import java.util.List;
+
 public class User {
+
+    private Long id;
+    private String name;
+    private Account account;
+    private Card card;
+    private List<Feature> features;
+    private List<News> newsList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<News> getNewsList() {
+        return newsList;
+    }
+
+    public void setNewsList(List<News> newsList) {
+        this.newsList = newsList;
+    }
 }
