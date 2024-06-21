00335443e7d48ddca5bd61a50722349b4366df1e
Bowie <marketing@letscode.com.br>
2024-06-21 17:12:59 -0300

    implementado a camada de dominio.

--- src/main/java/me/dio/santander_bootcamp_java/domain/model/Card.java
+++ src/main/java/me/dio/santander_bootcamp_java/domain/model/Card.java
@@ -11,7 +11,7 @@ public class Card {
     private Long id;
     @Column(unique = true)
     private String number;
-    @Column(name = "available_limit", scale = 13, precision = 2)
+    @Column(name = "available_limit", scale = 2, precision = 13)
     private BigDecimal limit;
 
     public Long getId() {
